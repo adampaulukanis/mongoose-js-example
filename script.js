b7dc91f21fc35b351723831e9f412f1df2836cd0
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-11-24 17:45:22 +0100

    2nd commit

--- script.js
+++ script.js
@@ -13,10 +13,32 @@ mongoose.connect(
   }
 );
 
-async function run () {
-  const user = new User({ name: 'Adam', age: 35 });
-  await user.save();
-  console.log(await User.find({ name: 'Adam' }));
+async function run() {
+  try {
+    // const user = new User({ name: 'Adam', age: 35 });
+    const user = await User.create({
+      name: 'Adam',
+      age: 2021 - 1985,
+      hobbies: ['Travelling', 'Sport'],
+      address: {
+        street: 'Main st.',
+      },
+      email: 'ADAM@x.filez',
+    });
+    user.name = 'Sadam';
+    await user.save();
+
+    user.age = 20;
+    user.hobbies.push('kaszanka');
+    user.email = 'sadam@world.pl';
+    user.createdAt = 5;
+    await user.save();
+
+    console.log(await User.find({ name: 'Sadam' }));
+    //console.log(await User.find({ name: 'Adam' }));
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 run();
